05d6414bf4cb012c587986592bbc843eb825ec95
dev-sibli <siblisadik13@gmail.com>
2022-02-17 18:22:17 +0600

    calculate functiod declare & call

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
+function getInputValue(inputId) {
+	const inputValue = document.getElementById(inputId).value;
+	const convertedValue = parseFloat(inputValue);
+	return convertedValue;
+}
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
 	const income = getInputValue('income-field');
-	console.log(income);
+	const food = getInputValue('food-field');
+	const rent = getInputValue('rent-field');
+	const clothes = getInputValue('clothes-field');
+	const totalExpense = document.getElementById('total-expense');
+	const balance = document.getElementById('balance');
+	const totalCost = food + rent + clothes;
+	totalExpense.innerText = totalCost;
+	balance.innerText = income - totalCost;
 });
